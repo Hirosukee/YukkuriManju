e5f62f1376e85e5ea518cfc3ef2d7dd1e3a240ef
BuildTools <unconfigured@null.spigotmc.org>
2021-08-31 20:47:11 +0900

    add recipe & add texture

--- src/main/java/hirosuke/YukkuriManju.java
+++ src/main/java/hirosuke/YukkuriManju.java
@@ -6,8 +6,10 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 
 @Mod(modid=YukkuriManju.modid, name = YukkuriManju.name, version = YukkuriManju.version)
 public class YukkuriManju {
@@ -18,6 +20,7 @@ public class YukkuriManju {
     public static Item itemReimu;
     public static Item itemMarisa;
 
+    @Mod.EventHandler
     public void perInit(FMLPreInitializationEvent event) {
         itemReimu = new ItemFood(7, 5.0f, true)
                 .setCreativeTab(CreativeTabs.tabFood)
@@ -32,5 +35,11 @@ public class YukkuriManju {
         GameRegistry.registerItem(itemMarisa, "yukkurimarisa");
         LanguageRegistry.addName(itemReimu, "Yukkuri Reimu");
         LanguageRegistry.addName(itemMarisa, "Yukkuri Marisa");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        GameRegistry.addRecipe(new ItemStack(itemReimu), "ABA", "BAB", "ABA", 'A', Items.apple, 'B', Items.beef);
+        GameRegistry.addSmelting(new ItemStack(itemReimu), new ItemStack(itemMarisa), 0);
     }
 }
